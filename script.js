702e06211c018a2d2ce4d22cab61ff0a8c4ceed8
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-10-22 16:55:30 -0700

    added variables and tested with console log

--- script.js
+++ script.js
@@ -17,12 +17,27 @@ var numbers = ['0123456789'];
 
 var desiredChars = [];
 
+var useLower = false
+var useUpper = false
+var useSpecial = false
+var useNumber = false
+
 function generatePassword() {
   var passwordLength = Number(window.prompt("Enter desired length of password:"));
   passwordLength = parseInt (passwordLength);
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128); {
-    return "Please choose a number between 8 and 128.";
-  } 
+
+  useLower = confirm("Should your password include lower case letters?");
+  useUpper = confirm("Should your password include upper case letters?");
+  useSpecial = confirm("Should your password include special characters?");
+  useNumber = confirm("Should your password include numbers?");
+
+  // Testing if variables are working 
+  console.log(passwordLength)
+  console.log(useLower)
+  console.log(useUpper)
+  console.log(useSpecial)
+  console.log(useNumber)
+
 
 }
 
@@ -34,6 +49,11 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+useLower = false
+useUpper = false
+useSpecial = false
+useNumber = false
 
   passwordText.value = password;
 
