46a9e382863f0fc4fa638f32d1d4977b5fdc5ecf
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-10-22 17:55:25 -0700

    added response for condition in which user chooses no character types

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // To do list: 
 // create an array for each type of character (lower, upper, special, number) [x]
 // create a desiredChars array [x]
-// add user selected chars to desiredChars array [ ]
+// add user selected chars to desiredChars array [X]
 // pick one random element from each charType array [ ]
   // (to ensure we get at least one of each, per challenge requirements)
 // select a random element from desiredChars array [ ]
@@ -17,14 +17,18 @@ var numbers = ['0123456789'];
 
 var desiredChars = [];
 
-var useLower = false
-var useUpper = false
-var useSpecial = false
-var useNumber = false
+var useLower = false; 
+var useUpper = false; 
+var useSpecial = false; 
+var useNumber = false; 
 
 function generatePassword() {
   var passwordLength = Number(window.prompt("Enter desired length of password:"));
   passwordLength = parseInt (passwordLength);
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    return "Please choose a number between 8 and 128.";
+  }
 
   useLower = confirm("Should your password include lower case letters?");
   useUpper = confirm("Should your password include upper case letters?");
@@ -37,6 +41,10 @@ function generatePassword() {
   console.log(useUpper)
   console.log(useSpecial)
   console.log(useNumber)
+
+  if (!useLower && !useUpper && !useSpecial && !useNumber) {
+    return "Please choose at least one type of character."
+  }
 
   if (useLower === true) {
     desiredChars = desiredChars.concat(lowerCase)
